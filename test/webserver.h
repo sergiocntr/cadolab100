253d82d24fee3e5b41231d8a1806c1fab53aa46d
Sergio Contro <sergiocntr@gmail.com>
2021-12-19 10:55:01 +0100

    inviato 19-12

--- test/webserver.h
+++ test/webserver.h
@@ -5,9 +5,6 @@
 #include <ArduinoJson.h>
 #include "variabili.h"
 #include "html.h"
-void setupWeb(){
-  ip = WiFi.localIP();
-} 
 void Comanda_Uscita(uint8_t Numero)
 {
   if (uscita[Numero]==1)
@@ -20,8 +17,28 @@ void Comanda_Uscita(uint8_t Numero)
     //digitalWrite(Numero+1, LOW); 
     uscita[Numero]=1;
   }
+  
 }
-
+void faipagina(){
+        
+        p = (String)intestazione;
+        for ( int q = 1; q < 9; q++ )
+          {
+            
+            if (uscita[q]==1)            
+              { 
+                p+= "<button id=\"button" + String(q) + "menu\" href=\"#\" class=\"button button3\" type=\"button\" onclick= \"window.location.href='"; 
+              }
+            else
+              { p+= "<button id=\"button" + String(q) + "menu\" href=\"#\" class=\"button button4\" type=\"button\" onclick= \"window.location.href='"; }
+            p+= relcmd[q * 2];
+            p+= "'\" >";
+            p+= myStringsOn[q-1];
+            p+= "</button>";
+          }        
+          p += (String)finePagina;
+          p += (String)script;
+}
 void Pubblica_Web() 
 { 
     WiFiClient client = server.available();
@@ -52,7 +69,7 @@ void Pubblica_Web()
     if(req == "/status"){
       Serial.println("dentro status"); 
         //faiJson();
-        DynamicJsonDocument doc(1024);
+        DynamicJsonDocument doc(384);
         int rssi = WiFi.RSSI();
         if (rssi<-30&&rssi>-66) doc["rssiPer"] = "100%";
         else if (rssi<-67&&rssi>-69) doc["rssiPer"] = "90%"; 
@@ -63,63 +80,33 @@ void Pubblica_Web()
         doc["vers"] = Versione;
         doc["ssid"] = ssid;
         doc["rssi"] = rssi;
-
-
         JsonArray puls = doc.createNestedArray("puls");
         for (size_t i = 1; i < 9; i++)
         {
           puls.add(uscita[i]); 
           //Serial.println(uscita[i]);
         }
-        String json;
-        serializeJson(doc, json);
+        p="";   //reinizzializzo variabile
+        serializeJson(doc, p);  //qui la pagina viene fatta dalla libreria arduinoJson
         client.println("HTTP/1.1 200 OK");
-        client.println("Content-type:text/html");
-        client.println("Connection: close");
-        client.println();
-        client.println();
-        client.println(json);
-        
-    }else {
-      
-      bool check =false;
-      if (req.endsWith("/e")) {
-        Comanda_Uscita(0);
-        client.println((const char*)index_min_html);
-      } else
-      if (req.endsWith("/t")) {
-        Comanda_Uscita(1);
-        client.println((const char*)index_min_html);
-      } else
-      if (req.endsWith("/u")) {
-        Comanda_Uscita(2);
-        client.println((const char*)index_min_html);
-      } else
-      if (req.endsWith("/o")) {
-        Comanda_Uscita(3);
-        client.println((const char*)index_min_html);
-      } else
-      if (req.endsWith("/a")) {
-        Comanda_Uscita(4);
-        client.println((const char*)index_min_html);
-      } else
-      if (req.endsWith("/d")) {
-        Comanda_Uscita(5);
-        client.println((const char*)index_min_html);
-      } else
-      if (req.endsWith("/g")) {
-        Comanda_Uscita(6);
-        client.println((const char*)index_min_html);
-      } else
-      if (req.endsWith("/j")) {
-        Comanda_Uscita(7);
-        client.println((const char*)index_min_html);
-      }
-      else if (req == "/")
-      {
-        client.println((const char*)index_min_html);
-      }
+        client.println("Content-type:application/json");
+    }else{
+    if (req.endsWith("/e")) Comanda_Uscita(1);
+    else if (req.endsWith("/t")) Comanda_Uscita(2);
+    else if (req.endsWith("/u")) Comanda_Uscita(3);
+    else if (req.endsWith("/o")) Comanda_Uscita(4);
+    else if (req.endsWith("/a")) Comanda_Uscita(5);
+    else if (req.endsWith("/d")) Comanda_Uscita(6);
+    else if (req.endsWith("/g")) Comanda_Uscita(7);
+    else if (req.endsWith("/j"))  Comanda_Uscita(8);
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-type:text/html");
+    client.println("Connection: close");
+    faipagina();
     }
+    client.println();
+    client.println();
+    client.println(p);
     client.println();
     client.stop();
     Serial.println("Chiuso client");
---
+++ test/wifi.h
@@ -0,0 +1,44 @@
+#pragma once
+#ifndef _wifi_h
+#define _wifi_h
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiClient.h>
+#include "variabili.h"
+#include "impostazioni.h"
+long rssi;
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
+void inizializza_wifi()
+{
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS))
+  {
+    Serial.println("STA Failed to configure");
+  }
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  if (!MDNS.begin("esp32"))
+  {
+    Serial.println("Error setting up MDNS responder!");
+    while (1)
+    {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  server.begin();
+  Serial.println("TCP server started");
+
+  MDNS.addService("http", "tcp", 80);
+}
+#endif
